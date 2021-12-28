89508b88f11e8868c423876a9a086bc46e05edab
DeabitTech <info@infomillennium2000.it>
2021-12-28 17:54:59 +0100

    commit sidebar paused

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,15 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import  {Container,Row} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+//icons
+import logoGrande from '../../assets/icons/logoGrandeSidebar.png';
+import addressAvatar from '../../assets/icons/addressAvatar.png';
+import tierPro from '../../assets/icons/tierPro.png';
+import logOutIcon from '../../assets/icons/logOutIcon.png';
+import * as Icon from 'react-bootstrap-icons'
+//icons
+import { useWeb3React } from '@web3-react/core';
+import useAuth from 'hooks/useAuth';
+
 const SideBar = () => {
+    const { account } = useWeb3React();
+    const {logout} = useAuth()
     const [tiers,setTiers] = useState(0);
+
     const sideBarRows = [
-        {title:"Dashboard",id:"dashboard"},
+        {title:"Dashboard",id:"dashboard", icon:require('../../assets/icons/logOutIcon.png')},
         {title:"Wallet",id:"wallet"},
         {title:"History",id:"history"},
         {title:"Trade",id:"trade"},
         {title:"Stacking",id:"stacking"},
         {title:"Tiers",id:"tiers"}
     ];
+    const getShrunkWalletAddress = (addr) => {
+        return (addr && `${addr.substring(0,4)}.....${addr.substring(addr.length-11)}`)
+    }
+    
+
     useEffect(()=>{
 
         
@@ -18,22 +37,72 @@ const SideBar = () => {
     return (
         <Container fluid className="sidebar-container">
             <div className="sidebar">
-            <Row className="logo">
+            <Row className="logo align-items-center">
+                <img src="images/logo.svg" alt="logo" />
+            </Row>
+            <Row className="mt-5">
+            <hr className="logo-under-line "/>
 
             </Row>
             <div className="menu">
-
-            {sideBarRows?.map((item,id)=>{
-                return (
-                    <Row key={id}>
-                    {item.title}
+                <Row className="menu-item">
+                    <Link to="dashboard" className="link">
+                        <Icon.HouseDoor/>
+                        Dashboard
+                    </Link>
                 </Row>
-               ) 
-            })}
+                <Row className="menu-item">
+                    <Link to="wallet" className="link">
+                        <Icon.CreditCard2Back/>
+                        Wallet
+                    </Link>
+                </Row>
+                <Row className="menu-item">
+                    <Link to="history" className="link">
+                        <Icon.ClockHistory/>
+                        History
+                    </Link>
+                </Row>
+                <Row className="menu-item">
+                    <Link to="trade" className="link">
+                        <Icon.CurrencyExchange/>
+                        Trade
+                    </Link>
+                </Row>
+                <Row className="menu-item">
+                    <Link to="stacking" className="link">
+                        <Icon.Stack/>
+                        Stacking
+                    </Link>
+                </Row>
+                <Row className="menu-item">
+                    <Link to="tiers" className="link">
+                        <Icon.LightningCharge/>
+                        Tiers
+                    </Link>
+                </Row>
             </div>
-            <Row className="footer">
-
+            <Row className="logoBig">
+                <img src={logoGrande} alt="logo"/>
             </Row>
+            <Container className="footer">
+                <Row className="addressSection">
+                    <img src={addressAvatar}/>    
+                    <div className="address">
+                    {getShrunkWalletAddress(account)}
+                    </div>
+                </Row>
+                <Row className="tierSection">
+                    <img src={tierPro}/>
+                </Row>    
+                <hr className="address-under-line "/>
+                <Row className="logoutSection" onClick={logout}>
+                    <img src={logOutIcon}/>
+                    <div className="logout-text">
+                        Log Out
+                    </div>
+                </Row>
+            </Container>
             </div>
         </Container>
     );
