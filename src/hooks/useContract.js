4ef97582d1a635a7000d0f2f763885d67b2031e2
DeabitTech <info@infomillennium2000.it>
2021-12-31 15:49:27 +0100

    fix exporting web3 from useWeb3

--- src/hooks/useContract.js
+++ src/hooks/useContract.js
@@ -3,16 +3,16 @@ import useWeb3 from 'hooks/useWeb3';
 import { getBep20Contract, getLpTokenContract, getStakePoolContract } from 'utils/contractHelpers';
 
 export const useStakePool = poolId => {
-  const web3 = useWeb3();
+  const {web3} = useWeb3();
   return useMemo(() => getStakePoolContract(poolId, web3), [poolId, web3]);
 };
 
 export const useERC20 = address => {
-  const web3 = useWeb3();
+  const {web3}= useWeb3();
   return useMemo(() => getBep20Contract(address, web3), [address, web3]);
 };
 
 export const useLPToken = address => {
-  const web3 = useWeb3();
+  const {web3} = useWeb3();
   return useMemo(() => getLpTokenContract(address, web3), [address, web3]);
 };
