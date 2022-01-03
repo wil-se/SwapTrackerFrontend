ab8a7bb9498f6beb9bc9c9a303643d7c9313f51f
William <sebastiani.1753672@studenti.uniroma1.it>
2022-01-03 18:57:53 +0100

    fix

--- src/utils/getBusdOut.js
+++ src/utils/getBusdOut.js
@@ -6,16 +6,12 @@ const BNB = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c".toLowerCase()
 const BUSD = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56".toLowerCase()
 
 
-
-export async function getBusdOut(tokenName, tokenAddress, amount){
-    if(tokenName === "BUSD") return amount
+export async function getBusdOut(tokenAddress, amount){
+    if(tokenAddress === BUSD) return amount
     var path = [tokenAddress.toLowerCase(),]
     if(tokenAddress.toLowerCase() != BNB) path.push(BNB)
     path.push(BUSD)
-    console.log(path)
-    let base_amt = new BigNumber(1).shiftedBy(18);
     var out = await getContract(PancakeAbi, "0x10ED43C718714eb63d5aA57B78B54704E256024E".toLowerCase())
-    .methods.getAmountsOut(base_amt, path).call()
-    console.log(out)
+    .methods.getAmountsOut(new BigNumber(1).shiftedBy(18), path).call()
     return out[out.length - 1]
 }
