d42c4cdf665db8f485c939b065995683e8413499
William <sebastiani.1753672@studenti.uniroma1.it>
2022-01-17 06:50:46 +0100

    tiers done

---
+++ src/hooks/useWalletConnectAuth.js
@@ -0,0 +1,59 @@
+
+import WalletConnect from "@walletconnect/client";
+import QRCodeModal from "@walletconnect/qrcode-modal";
+import { useWeb3React, UnsupportedChainIdError,handleAccountsChanged } from '@web3-react/core';
+import { useCallback, useEffect, useRef } from 'react';
+
+
+const connector = new WalletConnect({
+  bridge: "https://bridge.walletconnect.org", // Required
+  qrcodeModal: QRCodeModal,
+});
+
+
+const useWalletConnectAuth = () => {
+  const { activate, deactivate } = useWeb3React();
+  const walletConnectLogin = useCallback(() =>{
+    console.log("haloo");
+
+    activate(connector, async error => {
+       // Check if connection is already established
+       console.log(connector);
+       if (!connector.connected) {
+          // create new session
+          console.log("newsession");
+          connector.createSession();
+          console.log("new session ok");
+        }
+      
+        // Subscribe to connection events
+        connector.on("connect", (error, payload) => {
+          if (error) {
+            throw error;
+          }
+        
+          // Get provided accounts and chainId
+          const { accounts, chainId } = payload.params[0];
+        });
+      
+        connector.on("session_update", (error, payload) => {
+          if (error) {
+            throw error;
+          }
+        
+          // Get updated accounts and chainId
+          const { accounts, chainId } = payload.params[0];
+        });
+      
+        connector.on("disconnect", (error, payload) => {
+          if (error) {
+          throw error;
+        }
+      })
+    })
+  })
+
+  return { walletConnectLogin }
+}
+
+export default useWalletConnectAuth;
--- src/pages/Tier.js
+++ src/pages/Tier.js
@@ -1,50 +1,65 @@
 import React, { useState,useEffect } from 'react'
 import MainContainer from 'components/MainContainer'
 import { Card, Row, Col } from 'react-bootstrap';
+import Stack0 from '../assets/icons/stack0.png';
+import Stack1 from '../assets/icons/stack1.png';
+import Stack2 from '../assets/icons/stack2.png';
+
 
 const Wallet = () => {
     return (
         <MainContainer>
+        <>
           <h1 className="subheader-title">Tiers</h1>
-          <p>Unlock one of the tiers to start using SwapTracker</p>
-          <Row>
-            <Col>
-              <Card className="text-center" style={{padding: 25}}>
-                <div style={{marginLeft: 85, marginRight: 85}}>
+          <p style={{color: "#8DA0B0", fontWeight: 800}}>Unlock one of the tiers to start using SwapTracker</p>
+          <Row style={{paddingLeft: 100, paddingRight: 100, paddingTop: 50}}>
+            <Col md={4}>
+              <Card className="text-center card-no-border" style={{padding: 25}}>
+                <div style={{marginLeft: 70, marginRight: 70}}>
                   <h4 className="font-weight-bold">STARTER</h4>
+                  <Row style={{marginTop: 30}}><Col><img src={Stack0} /></Col></Row>
+                  <Row style={{marginTop: -14, visibility: 'hidden'}}><Col><img src={Stack1} /></Col></Row>
+                  <Row style={{marginBottom: 10, marginTop: -14, visibility: 'hidden'}}><Col><img src={Stack2} /></Col></Row>
                   <p className="font-weight-bold">Must hold 1.000.000 SWPT in your connected wallet</p>
                 </div>
                 <hr/>
-                <div style={{marginLeft: 100, marginRight: 100}}>
+                <div style={{marginLeft: 90, marginRight: 90, marginTop: 30}}>
                   <p className="font-weight-bold">All features available 1% transaction fee</p>
                 </div>
               </Card>
             </Col>
-            <Col>
-              <Card className="text-center" style={{padding: 25}}>
-                <div style={{marginLeft: 85, marginRight: 85}}>
+            <Col md={4}>
+              <Card className="text-center card-no-border" style={{padding: 25}}>
+                <div style={{marginLeft: 70, marginRight: 70}}>
                   <h4 className="font-weight-bold">ADVANCED</h4>
+                  <Row style={{marginTop: 30}}><Col><img src={Stack0} /></Col></Row>
+                  <Row style={{marginTop: -14}}><Col><img src={Stack1} /></Col></Row>
+                  <Row style={{marginBottom: 10, marginTop: -14, visibility: 'hidden'}}><Col><img src={Stack2} /></Col></Row>
                   <p className="font-weight-bold">Must hold 5.000.000 SWPT in your connected wallet</p>
                 </div>
                 <hr/>
-                <div style={{marginLeft: 100, marginRight: 100}}>
+                <div style={{marginLeft: 90, marginRight: 90, marginTop: 30}}>
                   <p className="font-weight-bold">All features available 0.5% transaction fee</p>
                 </div>
               </Card>
             </Col>
-            <Col>
-              <Card className="text-center" style={{padding: 25}}>
-              <div style={{marginLeft: 85, marginRight: 85}}>
+            <Col md={4}>
+              <Card className="text-center card-no-border tier-card-active" style={{padding: 25}}>
+              <div style={{marginLeft: 65, marginRight: 65}}>
                   <h4 className="font-weight-bold">PRO</h4>
+                  <Row style={{marginTop: 30}}><Col><img src={Stack0} /></Col></Row>
+                  <Row style={{marginTop: -14}}><Col><img src={Stack1} /></Col></Row>
+                  <Row style={{marginBottom: 10, marginTop: -14}}><Col><img src={Stack2} /></Col></Row>
                   <p className="font-weight-bold">Must hold 10.000.000 SWPT in your connected wallet</p>
                 </div>
                 <hr/>
-                <div style={{marginLeft: 100, marginRight: 100}}>
+                <div style={{marginLeft: 90, marginRight: 90, marginTop: 30}}>
                   <p className="font-weight-bold">All features available 0% transaction fee</p>
                 </div>
               </Card>
             </Col>
           </Row>
+          </>
         </MainContainer>
     )
 }
