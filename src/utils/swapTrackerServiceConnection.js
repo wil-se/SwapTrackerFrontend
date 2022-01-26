7d4d3aef40743de320f84ee7e6652f41b810945a
William <sebastiani.1753672@studenti.uniroma1.it>
2022-01-26 20:41:37 +0100

    test

--- src/utils/swapTrackerServiceConnection.js
+++ src/utils/swapTrackerServiceConnection.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://54.38.188.69:8080",
-  
+  // baseURL: "http://54.38.188.69:8080",
+  baseURL: "https://api.swaptracker.io",
 });
 
 export const callPost = (action,body) => {
