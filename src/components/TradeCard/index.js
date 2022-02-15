21251af66d2ffa0ee0ff440b26dc6a76c9fffd48
unknown <filippo.sofi.res@gmail.com>
2022-02-15 15:28:08 +0100

    trade responsive

--- src/components/TradeCard/index.js
+++ src/components/TradeCard/index.js
@@ -18,7 +18,7 @@ const TradeCard = () => {
     },[account])
 
      return (
-        <div className="trade-card-container"> 
+        <div className="trade-card-container d-flex justify-content-center">
             <TradeMainCard tier={tier}/>
         </div>
     )
