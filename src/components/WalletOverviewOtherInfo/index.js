805dc16ea0b6b34e10f1fae16b53334a97f63588
William <sebastiani.1753672@studenti.uniroma1.it>
2022-02-20 23:10:53 +0000

    fixes

--- src/components/WalletOverviewOtherInfo/index.js
+++ src/components/WalletOverviewOtherInfo/index.js
@@ -6,14 +6,15 @@ import { Card, Row, Col } from 'react-bootstrap';
 
 export function WalletOverviewOtherInfo(props){
   return(
-<Col xs={6} className="m-0" style={{border: "1px solid #ACD8E6", borderRadius: 10}}>
-
+<Col xs={6} className="m-0 p-1">
+<Col  style={{border: "1px solid #ACD8E6", borderRadius: 10}}>
 <Col className="mt-1">
           <a style={{fontSize: 14, fontWeight: 800}}>OTHER</a>
         </Col>
         <Col className="mt-1 d-flext text-right pr-2">
           <span className="align-items-center" style={{fontSize: 14, fontWeight: 800}}>{props.otherPerc} %</span>
         </Col>
+</Col>
 </Col>
   )
 }
