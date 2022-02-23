4bd766972ef0c6fbd409fb438f9c6192c07b2357
DeabitTech <info@infomillennium2000.it>
2022-02-23 17:09:47 +0100

    fixes

--- src/utils/web3.js
+++ src/utils/web3.js
@@ -1,6 +1,7 @@
 import Web3 from 'web3';
 import getRpcUrl from 'utils/getRpcUrl';
 import WalletConnect from "@walletconnect/client";
+import QRCodeModal from "@walletconnect/qrcode-modal";
 const RPC_URL = getRpcUrl();
 const httpProvider = new Web3.providers.HttpProvider(RPC_URL, { timeout: 10000 });
 const web3NoAccount = new Web3(httpProvider);
@@ -10,6 +11,7 @@ const getWeb3NoAccount = () => web3NoAccount;
 const getWeb3WalletConnect = () => {
     const provider = new WalletConnect({   
         bridge: "https://bridge.walletconnect.org",   
+        qrcodeModal: QRCodeModal,
         rpc:{
             56:RPC_URL
         }
